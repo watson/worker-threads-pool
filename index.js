0f2844cc03ad8c13bcc613f26339fbf054d09ed4
Thomas Watson <w@tson.dk>
2018-06-24 19:36:03 +0200

    Use ES 2015 class syntax #yolo

--- index.js
+++ index.js
@@ -5,51 +5,47 @@ const afterAll = require('after-all')
 
 const noop = function () {}
 
-module.exports = Pool
+module.exports = class Pool {
+  constructor (opts) {
+    opts = opts || {}
+    this._workers = new Set()
+    this._queue = []
+    this._max = opts.max || 1
+  }
 
-function Pool (opts) {
-  opts = opts || {}
-  this._workers = new Set()
-  this._queue = []
-  this._max = opts.max || 1
+  get size () {
+    return this._workers.size
+  }
 
-  Object.defineProperty(this, 'size', {
-    configurable: true,
-    enumerable: true,
-    get: function () {
-      return this._workers.size
+  acquire (filename, opts, cb) {
+    if (this._workers.size === this._max) {
+      this._queue.push([filename, opts, cb])
+      return
     }
-  })
-}
 
-Pool.prototype.acquire = function (filename, opts, cb) {
-  if (this._workers.size === this._max) {
-    this._queue.push([filename, opts, cb])
-    return
+    const self = this
+
+    const worker = new Worker(filename, opts)
+    worker.once('error', done)
+    worker.once('exit', done)
+
+    this._workers.add(worker)
+
+    process.nextTick(cb.bind(null, worker))
+
+    function done () {
+      self._workers.delete(worker)
+      worker.removeListener('error', done)
+      worker.removeListener('exit', done)
+      const next = self._queue.shift()
+      if (next) self.acquire.apply(self, next)
+    }
   }
 
-  const self = this
-
-  const worker = new Worker(filename, opts)
-  worker.once('error', done)
-  worker.once('exit', done)
-
-  this._workers.add(worker)
-
-  process.nextTick(cb.bind(null, worker))
-
-  function done () {
-    self._workers.delete(worker)
-    worker.removeListener('error', done)
-    worker.removeListener('exit', done)
-    const next = self._queue.shift()
-    if (next) self.acquire.apply(self, next)
-  }
-}
-
-Pool.prototype.destroy = function (cb = noop) {
-  const next = afterAll(cb)
-  for (let worker of this._workers) {
-    worker.terminate(next())
+  destroy (cb = noop) {
+    const next = afterAll(cb)
+    for (let worker of this._workers) {
+      worker.terminate(next())
+    }
   }
 }
